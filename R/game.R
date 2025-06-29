4445b5a7f1d882b3c08e53cc45f0d0b1146bd992
Nils Brandenburg <nils.brandenburg@hhu.de>
2025-06-29 21:01:13 +0200

    feature: Add CashGame class
    Player submits answer. If correct, player is awarded the value of the question to their current score. If incorrect, all other players are awarded the value of the question

---
+++ R/game.R
@@ -0,0 +1,59 @@
+CashGame <- R6::R6Class(
+
+  "CashGame",
+
+  public = list(
+    initialize = \(
+      questions,
+      players
+    ) {
+      assert_questions(questions)
+      assert_players(players)
+
+      private$.uuid <- ids::uuid(n = 1L, drop_hyphens = TRUE)
+
+      private$.questions <- if (!is.list(questions)) list(questions) else questions
+      private$.players   <- if (!is.list(players))   list(players)   else players
+    },
+
+    submit = \(
+      id_question,
+      id_option,
+      id_player
+    ) {
+      question   <- get_question(self$questions, id_question)
+      is_correct <- question$submit(id_option)
+
+      selection <- if (is_correct) get_player else discard_player
+
+      players_to_update <- selection(self$players, id_player)
+
+      purrr::walk(
+        players_to_update,
+        \(e) {
+          e$update(10.0)
+        }
+      )
+    }
+  ),
+
+  private = list(
+    .uuid = NULL,
+    .questions = NULL,
+    .players = NULL
+  ),
+
+  active = list(
+    id = \() {
+      return(private$.uuid)
+    },
+
+    questions = \() {
+      return(private$.questions)
+    },
+
+    players = \() {
+      return(private$.players)
+    }
+  )
+)
