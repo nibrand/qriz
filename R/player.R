e89f20874f766332ab0924b3b2384c706f4ecd09
Nils Brandenburg <nils.brandenburg@hhu.de>
2025-06-29 20:59:04 +0200

    feature: Add player class
    Implements subscriber interface in accordance to the observer pattern

---
+++ R/player.R
@@ -0,0 +1,109 @@
+Player <- R6::R6Class(
+
+  "Player",
+
+  inherit = ISubscriber,
+
+  public = list(
+
+    initialize = \(name) {
+      checkmate::assert_character(name, len = 1, any.missing = FALSE, null.ok = FALSE)
+
+      private$.uuid  <- ids::uuid(n = 1L, drop_hyphens = TRUE)
+      private$.name  <- name
+      private$.score <- 0.0
+
+      invisible(self)
+    },
+
+    update = \(...) {
+      super$update(...)
+      args <- list(...)
+
+      old_score <- private$.score
+      new_score <- old_score + args$score_increment
+
+      private$.score <- new_score
+
+      invisible(old_score)
+    }
+  ),
+
+  private = list(
+    .uuid  = NULL,
+    .name  = NULL,
+    .score = NULL
+  ),
+
+  active = list(
+    id = \() {
+      return(private$.uuid)
+    },
+
+    name = \() {
+      return(private$.name)
+    },
+
+    score = \() {
+      return(private$.score)
+    }
+  )
+)
+
+
+
+assert_players <- function(x) {
+
+  if (!is.list(x)) {
+    return(checkmate::assert_class(x, "Player"))
+  }
+
+  purrr::walk(
+    x,
+    \(e) {
+      checkmate::assert_class(e, "Player")
+    }
+  )
+}
+
+
+
+get_player <- function(x, id) {
+  checkmate::assert_list(x)
+
+  res <- purrr::keep(
+    x,
+    \(e) {
+      e$id == id
+    }
+  )
+
+  if (length(res) == 0L) {
+    rlang::abort(glue::glue(
+      "Unable to find player with id {id}"
+    ))
+  }
+
+  res
+}
+
+
+
+discard_player <- function(x, id) {
+  checkmate::assert_list(x)
+
+  res <- purrr::discard(
+    x,
+    \(e) {
+      e$id == id
+    }
+  )
+
+  if (length(res) == length(x)) {
+    rlang::abort(glue::glue(
+      "Unable to find player with id {id}"
+    ))
+  }
+
+  res
+}
---
+++ R/subscriber.R
@@ -0,0 +1,28 @@
+ISubscriber <- R6::R6Class(
+
+  "ISubscriber",
+
+  public = list(
+    # implement constructor to block any attempt of initializing an interface object
+    initialize = \() {
+      rlang::abort(
+        "ISubscriber is an interface that cannot be initialized on its own",
+        class = "interface_instance"
+      )
+    },
+
+    update = \(score_increment) {
+      is_bad_implementation <-
+        checkmate::test_number(score_increment)
+
+      if (is_bad_implementation) {
+        rlang::abort(
+          "Invalid implementation of ISubscriber",
+          class = "interface_invalid_implementation"
+        )
+      }
+
+      invisible(NULL)
+    }
+  )
+)
