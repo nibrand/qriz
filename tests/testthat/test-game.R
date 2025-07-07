c96a70cc752cd7637283b8c71f03d171bc92a40f
Nils Brandenburg <nils.brandenburg@hhu.de>
2025-07-07 21:21:53 +0200

    test: Add unit-tests for CashGame

---
+++ tests/testthat/test-game.R
@@ -0,0 +1,239 @@
+testthat::test_that("Player in CashGame gets rewarded after submitting correct answer", {
+  mc_options <- list(
+    list(
+      id = "1.0001",
+      label = "foo",
+      is_correct = TRUE
+    ),
+    list(
+      id = "2.0001",
+      label = "bar",
+      is_correct = FALSE
+    )
+  )
+
+  question <- QuestionMultipleChoice$new(
+    prompt     = "foo",
+    value      = 10.0,
+    mc_options = mc_options
+  )
+
+  player <- Player$new("Bob")
+
+  id_question <- question$id
+  id_player   <- player$id
+
+  game <- CashGame$new(questions = list(question), players = list(player))
+
+  game$submit(
+    id_question = id_question,
+    id_option   = "1.0001",
+    id_player   = id_player
+  )
+
+  testthat::expect_equal(player$score, 10.0)
+})
+
+
+
+testthat::test_that("Player in CashGame gets no reward after submitting incorrect answer", {
+  mc_options <- list(
+    list(
+      id = "1.0001",
+      label = "foo",
+      is_correct = TRUE
+    ),
+    list(
+      id = "2.0001",
+      label = "bar",
+      is_correct = FALSE
+    )
+  )
+
+  question <- QuestionMultipleChoice$new(
+    prompt     = "foo",
+    value      = 10.0,
+    mc_options = mc_options
+  )
+
+  player <- Player$new("Bob")
+
+  id_question <- question$id
+  id_player   <- player$id
+
+  game <- CashGame$new(questions = list(question), players = list(player))
+
+  game$submit(
+    id_question = id_question,
+    id_option   = "2.0001",
+    id_player   = id_player
+  )
+
+  testthat::expect_equal(player$score, 0.0)
+})
+
+
+
+testthat::test_that("Player in CashGame gets multiple rewards after submitting correct answers back-to-back", {
+  mc_options_1 <- list(
+    list(
+      id = "1.0001",
+      label = "foo",
+      is_correct = TRUE
+    ),
+    list(
+      id = "2.0001",
+      label = "bar",
+      is_correct = FALSE
+    )
+  )
+
+  mc_options_2 <- list(
+    list(
+      id = "1.0002",
+      label = "foo",
+      is_correct = TRUE
+    ),
+    list(
+      id = "2.0002",
+      label = "bar",
+      is_correct = FALSE
+    )
+  )
+
+  question_1 <- QuestionMultipleChoice$new(
+    prompt     = "foo",
+    value      = 10.0,
+    mc_options = mc_options_1
+  )
+
+  question_2 <- QuestionMultipleChoice$new(
+    prompt     = "bar",
+    value      = 20.0,
+    mc_options = mc_options_2
+  )
+
+  player <- Player$new("Bob")
+
+  id_question_1 <- question_1$id
+  id_question_2 <- question_2$id
+  id_player     <- player$id
+
+  game <- CashGame$new(questions = list(question_1, question_2), players = list(player))
+
+  game$submit(
+    id_question = id_question_1,
+    id_option   = "1.0001",
+    id_player   = id_player
+  )
+
+  testthat::expect_equal(player$score, 10.0)
+
+  game$submit(
+    id_question = id_question_2,
+    id_option   = "1.0002",
+    id_player   = id_player
+  )
+
+  testthat::expect_equal(player$score, 30.0)
+})
+
+
+
+testthat::test_that("Player in CashGame gets no reward after submitting correct answer to solved question", {
+  mc_options <- list(
+    list(
+      id = "1.0001",
+      label = "foo",
+      is_correct = TRUE
+    ),
+    list(
+      id = "2.0001",
+      label = "bar",
+      is_correct = FALSE
+    )
+  )
+
+  question <- QuestionMultipleChoice$new(
+    prompt     = "foo",
+    value      = 10.0,
+    mc_options = mc_options
+  )
+
+  player_1 <- Player$new("Bob")
+  player_2 <- Player$new("Alice")
+
+  id_question <- question$id
+  id_player_1 <- player_1$id
+  id_player_2 <- player_2$id
+
+  game <- CashGame$new(questions = list(question), players = list(player_1, player_2))
+
+  game$submit(
+    id_question = id_question,
+    id_option   = "1.0001",
+    id_player   = id_player_1
+  )
+
+  testthat::expect_equal(player_1$score, 10.0)
+  testthat::expect_equal(player_2$score,  0.0)
+
+  game$submit(
+    id_question = id_question,
+    id_option   = "1.0001",
+    id_player   = id_player_2
+  )
+
+  testthat::expect_equal(player_1$score, 10.0)
+  testthat::expect_equal(player_2$score,  0.0)
+})
+
+
+
+testthat::test_that("Player in CashGame does not earn reward for others after submitting incorrect answer to solved question", {
+  mc_options <- list(
+    list(
+      id = "1.0001",
+      label = "foo",
+      is_correct = TRUE
+    ),
+    list(
+      id = "2.0001",
+      label = "bar",
+      is_correct = FALSE
+    )
+  )
+
+  question <- QuestionMultipleChoice$new(
+    prompt     = "foo",
+    value      = 10.0,
+    mc_options = mc_options
+  )
+
+  player_1 <- Player$new("Bob")
+  player_2 <- Player$new("Alice")
+
+  id_question <- question$id
+  id_player_1 <- player_1$id
+  id_player_2 <- player_2$id
+
+  game <- CashGame$new(questions = list(question), players = list(player_1, player_2))
+
+  game$submit(
+    id_question = id_question,
+    id_option   = "1.0001",
+    id_player   = id_player_1
+  )
+
+  testthat::expect_equal(player_1$score, 10.0)
+  testthat::expect_equal(player_2$score,  0.0)
+
+  game$submit(
+    id_question = id_question,
+    id_option   = "2.0001",
+    id_player   = id_player_2
+  )
+
+  testthat::expect_equal(player_1$score, 10.0)
+  testthat::expect_equal(player_2$score,  0.0)
+})
