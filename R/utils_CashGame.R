807f58aa9fa6d3edc29961212c8692bd526446a1
Nils Brandenburg <nils.brandenburg@hhu.de>
2025-07-21 20:44:11 +0200

    feature: Load custom game files with questions and UI texts
    Quiz data is stored in JSON format. Fed into the context API in launch function

--- R/utils_CashGame.R
+++ R/utils_CashGame.R
@@ -1,12 +1,43 @@
-# use_context
-#
-# set_context <- function(key, value) {
-#
-# }
+launch_CashGame <- function(file = NULL) {
+
+  if (is.null(file)) {
+    launch_CashGame_debug()
+    return(NULL)
+  }
+
+  file <- path_CashGame(file)
+
+  checkmate::assert_file_exists(file)
+
+  meta_data <- jsonlite::fromJSON(file, simplifyDataFrame = FALSE)
+
+  welcome <- shiny::HTML(meta_data$txt_welcome)
+
+  questions <- meta_data$questions |>
+    purrr::map(
+      \(e) {
+        QuestionMultipleChoice$new(
+          prompt = e$prompt,
+          value = e$value,
+          category = e$category,
+          mc_options = e$mc_options
+        )
+      }
+    )
+
+
+  set_context("txt_welcome", welcome)
+  set_context("questions", questions)
+
+  shiny::runApp(
+    appDir = path_CashGame()
+  )
+}
 
 
 
-launch_CashGame <- function() {
+launch_CashGame_debug <- function() {
+  welcome <- "Wilkommen"
 
   questions <- list(
     mock_QuestionMultipleChoice(value = 10, category = "Geschichte"),
@@ -26,6 +57,7 @@ launch_CashGame <- function() {
     mock_QuestionMultipleChoice(value = 1,  category = "Politik")
   )
 
+  set_context("txt_welcome", welcome)
   set_context("questions", questions)
 
   shiny::runApp(
